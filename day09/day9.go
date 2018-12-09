3566f32998c8cd83a301e94870096eeeabda64c5
Glen Haber <Glen_Haber@comcast.com>
2018-12-09 03:17:33 -0500

    Day 9

---
+++ day09/day9.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+)
+
+type Marble struct {
+	id         int
+	next, prev *Marble
+}
+
+func play(numPlayers, numMarbles int) int {
+	players := make([]int, numPlayers)
+	currentPlayer := 0
+	current := &Marble{0, nil, nil}
+	current.next = current
+	current.prev = current
+	for i := 1; i <= numMarbles; i++ {
+		if i%23 == 0 {
+			remove := current.prev.prev.prev.prev.prev.prev.prev
+			players[currentPlayer] += i + remove.id
+			current = remove.next
+			remove.prev.next = remove.next
+			remove.next.prev = remove.prev
+		} else {
+			before := current.next
+			after := before.next
+			insert := &Marble{i, after, before}
+			before.next = insert
+			after.prev = insert
+			current = insert
+		}
+		currentPlayer = (currentPlayer + 1) % numPlayers
+	}
+	sort.Ints(players)
+	return players[len(players)-1]
+}
+
+func main() {
+	inputBytes, _ := ioutil.ReadFile("input/day09.txt")
+	inputString := strings.TrimSpace(string(inputBytes))
+	var numPlayers, numMarbles int
+	fmt.Sscanf(inputString, "%d players; last marble is worth %d points", &numPlayers, &numMarbles)
+
+	fmt.Println(play(numPlayers, numMarbles))
+	fmt.Println(play(numPlayers, numMarbles*100))
+}
