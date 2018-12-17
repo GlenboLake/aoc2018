880d3112325b505314b423a8de306473d3b7093a
Glen Haber <Glen_Haber@comcast.com>
2018-12-17 15:46:52 -0500

    Day 16

---
+++ day16/day16.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Op func(A, B, C int, regs []int)
+
+type Instruction struct {
+	OpCode  int
+	A, B, C int
+}
+
+func copyRegs(registers []int) []int {
+	result := make([]int, len(registers))
+	copy(result, registers)
+	return result
+}
+
+func addr(A, B, C int, regs []int) {
+	regs[C] = regs[A] + regs[B]
+}
+
+func addi(A, B, C int, regs []int) {
+	regs[C] = regs[A] + B
+}
+
+func mulr(A, B, C int, regs []int) {
+	regs[C] = regs[A] * regs[B]
+}
+
+func muli(A, B, C int, regs []int) {
+	regs[C] = regs[A] * B
+}
+
+func banr(A, B, C int, regs []int) {
+	regs[C] = regs[A] & regs[B]
+}
+
+func bani(A, B, C int, regs []int) {
+	regs[C] = regs[A] & B
+}
+
+func borr(A, B, C int, regs []int) {
+	regs[C] = regs[A] | regs[B]
+}
+
+func bori(A, B, C int, regs []int) {
+	regs[C] = regs[A] | B
+}
+
+func setr(A, _, C int, regs []int) {
+	regs[C] = regs[A]
+}
+
+func seti(A, _, C int, regs []int) {
+	regs[C] = A
+}
+
+func gtir(A, B, C int, regs []int) {
+	if A > regs[B] {
+		regs[C] = 1
+	} else {
+		regs[C] = 0
+	}
+}
+
+func gtri(A, B, C int, regs []int) {
+	if regs[A] > B {
+		regs[C] = 1
+	} else {
+		regs[C] = 0
+	}
+}
+
+func gtrr(A, B, C int, regs []int) {
+	if regs[A] > regs[B] {
+		regs[C] = 1
+	} else {
+		regs[C] = 0
+	}
+}
+
+func eqir(A, B, C int, regs []int) {
+	if A == regs[B] {
+		regs[C] = 1
+	} else {
+		regs[C] = 0
+	}
+}
+
+func eqri(A, B, C int, regs []int) {
+	if regs[A] == B {
+		regs[C] = 1
+	} else {
+		regs[C] = 0
+	}
+}
+
+func eqrr(A, B, C int, regs []int) {
+	if regs[A] == regs[B] {
+		regs[C] = 1
+	} else {
+		regs[C] = 0
+	}
+}
+
+var AllOps = []Op{
+	addr, addi,
+	mulr, muli,
+	banr, bani,
+	borr, bori,
+	setr, seti,
+	gtir, gtri, gtrr,
+	eqir, eqri, eqrr,
+}
+
+type Case struct {
+	Before, After   []int
+	OpCode, A, B, C int
+}
+
+func (c Case) Test() map[int]bool {
+	options := map[int]bool{}
+	for i, op := range AllOps {
+		if c.testOp(op) {
+			options[i] = true
+		}
+	}
+	return options
+}
+
+func (c Case) testOp(op Op) bool {
+	regCopy := copyRegs(c.Before)
+	op(c.A, c.B, c.C, regCopy)
+	for i := range regCopy {
+		if regCopy[i] != c.After[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func part1(cases []Case) int {
+	count := 0
+	for _, c := range cases {
+		if len(c.Test()) >= 3 {
+			count += 1
+		}
+	}
+	return count
+}
+
+func mapOpCodes(cases []Case) map[int]Op {
+	opCodes := map[int]Op{}
+
+	possibilities := map[int][]int{}
+	list := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	for i := 0; i < 16; i++ {
+		possibilities[i] = make([]int, 16)
+		copy(possibilities[i], list)
+	}
+
+	for _, c := range cases {
+		caseList := c.Test()
+		var ok []int
+		for _, p := range possibilities[c.OpCode] {
+			if caseList[p] {
+				ok = append(ok, p)
+			}
+		}
+		possibilities[c.OpCode] = ok
+	}
+	determined := map[int]bool{}
+	for len(determined) < 16 {
+		for k, v := range possibilities {
+			if len(v) == 1 {
+				determined[v[0]] = true
+			} else {
+				var newList []int
+				for _, x := range v {
+					if !determined[x] {
+						newList = append(newList, x)
+					}
+				}
+				possibilities[k] = newList
+			}
+		}
+	}
+
+	for k, v := range possibilities {
+		opCodes[k] = AllOps[v[0]]
+	}
+
+	return opCodes
+}
+
+func part2(cases []Case, program []Instruction) int {
+	opMap := mapOpCodes(cases)
+
+	registers := make([]int, 4)
+	for _, inst := range program {
+		opMap[inst.OpCode](inst.A, inst.B, inst.C, registers)
+	}
+
+	return registers[0]
+}
+
+func main() {
+	f, _ := os.Open("input/day16.txt")
+
+	scanner := bufio.NewScanner(bufio.NewReader(f))
+	var cases []Case
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			break
+		}
+		before := make([]int, 4)
+		var opCode, a, b, c int
+		after := make([]int, 4)
+		fmt.Sscanf(scanner.Text(), "Before: [%d, %d, %d, %d]", &before[0], &before[1], &before[2], &before[3])
+		scanner.Scan()
+		fmt.Sscanf(scanner.Text(), "%d %d %d %d", &opCode, &a, &b, &c)
+		scanner.Scan()
+		fmt.Sscanf(scanner.Text(), "After: [%d, %d, %d, %d]", &after[0], &after[1], &after[2], &after[3])
+		scanner.Scan()
+		cases = append(cases, Case{before, after, opCode, a, b, c})
+	}
+	var program []Instruction
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			continue
+		}
+		var op, a, b, c int
+		fmt.Sscanf(scanner.Text(), "%d %d %d %d", &op, &a, &b, &c)
+		program = append(program, Instruction{op, a, b, c})
+	}
+
+	fmt.Println(part1(cases))
+	fmt.Println(part2(cases, program))
+}
